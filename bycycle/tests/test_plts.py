c70a82561f857f47c9aba76d9c3855db651f2999
Ryan Hammonds <ryan.hammonds@utexas.edu>
2020-06-03 15:04:21 -0700

    test added for plot_cyclepoints

--- bycycle/tests/test_plts.py
+++ bycycle/tests/test_plts.py
@@ -1,10 +1,10 @@
 """Tests plotting."""
 
 import pytest
-
 from neurodsp.sim import sim_oscillation
 from bycycle.features import compute_features
-from bycycle.plts import plot_burst_detect_params
+from bycycle.plts import plot_burst_detect_params, plot_cyclepoints
+from bycycle.cyclepoints import find_zerox, find_extrema
 
 @pytest.mark.parametrize("only_result", [True, False])
 def test_plot_burst_detect_params(only_result):
@@ -33,3 +33,40 @@ def test_plot_burst_detect_params(only_result):
             assert param is not None
     else:
         assert fig is not None
+
+
+@pytest.mark.parametrize("plot_type",
+    [
+            'extrema',
+            'zerox',
+            'both',
+            pytest.param(None, marks=pytest.mark.xfail(raises=TypeError))
+    ]
+)
+def test_plot_cyclepoints(plot_type):
+    """Test plotting cyclepoints."""
+
+    # Simulate oscillating time series
+    n_seconds = 10
+    fs = 500
+    freq = 10
+
+    sig = sim_oscillation(n_seconds, fs, freq)
+
+    # Find extrema and zero-crossings
+    ps, ts = find_extrema(sig, fs, (8, 12))
+    zerox_rise, zerox_decay = find_zerox(sig, ps, ts)
+
+    if plot_type == 'both':
+        ax = plot_cyclepoints(sig, fs, extrema=(ps, ts), zerox=(zerox_rise, zerox_decay))
+    elif plot_type == 'extrema':
+        ax = plot_cyclepoints(sig, fs, extrema=(ps, ts))
+    elif plot_type == 'zerox':
+        ax = plot_cyclepoints(sig, fs, zerox=(zerox_rise, zerox_decay))
+    else:
+        ax = plot_cyclepoints(sig, fs)
+
+    assert ax is not None
+
+
+
