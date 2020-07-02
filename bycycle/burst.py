b3ea48eb94a42814fefc716912c4b1fa282905a6
ryanhammonds <ryan.hammonds@utexas.edu>
2020-07-02 14:36:21 -0700

    refactored burst and features to increase modularity

--- bycycle/burst.py
+++ bycycle/burst.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 from neurodsp.burst import detect_bursts_dual_threshold
 
+from bycycle.features import compute_burst_fraction
+
 ###################################################################################################
 ###################################################################################################
 
 pd.options.mode.chained_assignment = None
 
-def detect_bursts_cycles(df, sig, amp_fraction_threshold=0.,
-                         amp_consistency_threshold=.5,
+def detect_bursts_cycles(df_features, amplitude_fraction_threshold=0.,
+                         amplitude_consistency_threshold=.5,
                          period_consistency_threshold=.5,
                          monotonicity_threshold=.8,
                          n_cycles_min=3):
@@ -19,11 +21,9 @@ def detect_bursts_cycles(df, sig, amp_fraction_threshold=0.,
 
     Parameters
     ----------
-    df : pandas DataFrame
-        Dataframe of waveform features for individual cycles, trough-centered.
-    sig : 1d array
-        Signal used to compute monotonicity.
-    amp_fraction_threshold : float, optional, default: 0.
+    df_features : pandas DataFrame
+        Waveform features for individual cycles from :func:`~.compute_features`.
+    amplitude_fraction_threshold : float, optional, default: 0.
         The minimum normalized amplitude a cycle must have in order to be considered in an
         oscillation. Must be between 0 and 1.
 
@@ -31,7 +31,7 @@ def detect_bursts_cycles(df, sig, amp_fraction_threshold=0.,
         - .5 = the median amplitude across all cycles
         - 1 = the maximum amplitude across all cycles
 
-    amp_consistency_threshold : float, optional, default: 0.5
+    amplitude_consistency_threshold : float, optional, default: 0.5
         The minimum normalized difference in rise and decay magnitude to be considered as in an
         oscillatory mode. Must be between 0 and 1.
 
@@ -59,7 +59,7 @@ def detect_bursts_cycles(df, sig, amp_fraction_threshold=0.,
 
     Returns
     -------
-    df : pandas DataFrame
+    df_features : pandas DataFrame
         Same df as input, with an additional column (`is_burst`) to indicate if the cycle is part
         of an oscillatory burst, with additional columns indicating the burst detection parameters.
 
@@ -68,81 +68,53 @@ def detect_bursts_cycles(df, sig, amp_fraction_threshold=0.,
     * The first and last period cannot be considered oscillating if the consistency measures are used.
     """
 
-    # Compute normalized amplitude for all cycles
-    df['amp_fraction'] = df['volt_amp'].rank()/len(df)
-
-    # Compute amplitude consistency
-    cycles = len(df)
-    amp_consists = np.ones(cycles) * np.nan
-    rises = df['volt_rise'].values
-    decays = df['volt_decay'].values
-
-    for cyc in range(1, cycles-1):
-
-        consist_current = np.min([rises[cyc], decays[cyc]]) / np.max([rises[cyc], decays[cyc]])
-
-        if 'sample_peak' in df.columns:
-            consist_last = np.min([rises[cyc], decays[cyc-1]]) / np.max([rises[cyc], decays[cyc-1]])
-            consist_next = np.min([rises[cyc+1], decays[cyc]]) / np.max([rises[cyc+1], decays[cyc]])
-
-        else:
-            consist_last = np.min([rises[cyc-1], decays[cyc]]) / np.max([rises[cyc-1], decays[cyc]])
-            consist_next = np.min([rises[cyc], decays[cyc+1]]) / np.max([rises[cyc], decays[cyc+1]])
-
-        amp_consists[cyc] = np.min([consist_current, consist_next, consist_last])
-
-    df['amp_consistency'] = amp_consists
-
-    # Compute period consistency
-    period_consists = np.ones(cycles) * np.nan
-    periods = df['period'].values
-
-    for cyc in range(1, cycles-1):
-
-        consist_last = np.min([periods[cyc], periods[cyc-1]]) / \
-            np.max([periods[cyc], periods[cyc-1]])
-        consist_next = np.min([periods[cyc+1], periods[cyc]]) / \
-            np.max([periods[cyc+1], periods[cyc]])
-
-        period_consists[cyc] = np.min([consist_next, consist_last])
-
-    df['period_consistency'] = period_consists
-
-    # Compute monotonicity
-    monotonicity = np.ones(cycles) * np.nan
-
-    for idx, row in df.iterrows():
-
-        if 'sample_peak' in df.columns:
-            rise_period = sig[int(row['sample_last_trough']):int(row['sample_peak'])]
-            decay_period = sig[int(row['sample_peak']):int(row['sample_next_trough'])]
-
-        else:
-            decay_period = sig[int(row['sample_last_peak']):int(row['sample_trough'])]
-            rise_period = sig[int(row['sample_trough']):int(row['sample_next_peak'])]
-
-        decay_mono = np.mean(np.diff(decay_period) < 0)
-        rise_mono = np.mean(np.diff(rise_period) > 0)
-        monotonicity[idx] = np.mean([decay_mono, rise_mono])
-
-    df['monotonicity'] = monotonicity
-
     # Compute if each period is part of an oscillation
-    cycle_good_amp = df['amp_fraction'] > amp_fraction_threshold
-    cycle_good_amp_consist = df['amp_consistency'] > amp_consistency_threshold
-    cycle_good_period_consist = df['period_consistency'] > period_consistency_threshold
-    cycle_good_monotonicity = df['monotonicity'] > monotonicity_threshold
+    cycle_amp_burst = df_features['amplitude_fraction'] > amplitude_fraction_threshold
+    cycle_amp_consist_burst = df_features['amplitude_consistency'] > amplitude_consistency_threshold
+    cycle_period_consist_burst = df_features['period_consistency'] > period_consistency_threshold
+    cycle_monotonicity_burst = df_features['monotonicity'] > monotonicity_threshold
 
-    is_burst = cycle_good_amp & cycle_good_amp_consist & \
-        cycle_good_period_consist & cycle_good_monotonicity
+    is_burst = cycle_amp_burst & cycle_amp_consist_burst & \
+        cycle_period_consist_burst & cycle_monotonicity_burst
     is_burst[0] = False
     is_burst[-1] = False
 
-    df['is_burst'] = is_burst
-    df = _min_consecutive_cycles(df, n_cycles_min=n_cycles_min)
-    df['is_burst'] = df['is_burst'].astype(bool)
+    df_features['is_burst'] = is_burst
+    df_features = _min_consecutive_cycles(df_features, n_cycles_min=n_cycles_min)
+    df_features['is_burst'] = df_features['is_burst'].astype(bool)
 
-    return df
+    return df_features
+
+
+def detect_bursts_df_amp(df_features, burst_fraction_threshold=1, n_cycles_min=3):
+    """Determine which cycles in a signal are part of an oscillatory
+    burst using an amplitude thresholding approach.
+
+    Parameters
+    ----------
+    df_features : pandas DataFrame
+        Waveform features for individual cycles from :func:`~.compute_features`.
+    burst_fraction_threshold : int or float, optional, default: 1
+        Minimum fraction of a cycle to be indentified as a burst.
+    n_cycles_min : int, optional, default: 3
+        Minimum number of cycles to be identified as truly oscillating needed in a row in order
+        for them to remain identified as truly oscillating.
+
+    Returns
+    -------
+    df : pandas DataFrame
+        Same df as input, with an additional column to indicate
+        if the cycle is part of an oscillatory burst.
+    """
+
+    # Determine cycles that are defined as bursting throughout the whole cycle
+    df_features['is_burst'] = [frac >= burst_fraction_threshold for frac in
+                               df_features['burst_fraction']]
+
+    df_features = _min_consecutive_cycles(df_features, n_cycles_min=n_cycles_min)
+    df_features['is_burst'] = df_features['is_burst'].astype(bool)
+
+    return df_features
 
 
 def _min_consecutive_cycles(df_shape, n_cycles_min=3):
@@ -167,55 +139,3 @@ def _min_consecutive_cycles(df_shape, n_cycles_min=3):
     df_shape['is_burst'] = is_burst
 
     return df_shape
-
-
-def detect_bursts_df_amp(df, sig, fs, f_range, amp_threshes=(1, 2),
-                         n_cycles_min=3, filter_kwargs=None):
-    """Determine which cycles in a signal are part of an oscillatory
-    burst using an amplitude thresholding approach.
-
-    Parameters
-    ----------
-    df : pandas DataFrame
-        Dataframe of waveform features for individual cycles, trough-centered.
-    sig : 1d array
-        Time series.
-    fs : float
-        Sampling rate, Hz.
-    f_range : tuple of (float, float)
-        Frequency range (Hz) for oscillator of interest.
-    amp_threshes : tuple (low, high), optional, default: (1, 2)
-        Threshold values for determining timing of bursts.
-        These values are in units of amplitude (or power, if specified) normalized to
-        the median amplitude (value 1).
-    n_cycles_min : int, optional, default: 3
-        Minimum number of cycles to be identified as truly oscillating needed in a row in
-        order for them to remain identified as truly oscillating.
-    filter_kwargs : dict, optional
-        Keyword arguments to :func:`~neurodsp.filt.filter.filter_signal`.
-
-    Returns
-    -------
-    df : pandas DataFrame
-        Same df as input, with an additional column to indicate
-        if the cycle is part of an oscillatory burst.
-    """
-
-    # Detect bursts using the dual amplitude threshold approach
-    sig_burst = detect_bursts_dual_threshold(sig, fs, amp_threshes, f_range,
-                                             min_n_cycles=n_cycles_min, **filter_kwargs)
-
-    # Compute fraction of each cycle that's bursting
-    burst_fracs = []
-    for _, row in df.iterrows():
-        fraction_bursting = np.mean(sig_burst[int(row['sample_last_trough']):
-                                              int(row['sample_next_trough'] + 1)])
-        burst_fracs.append(fraction_bursting)
-
-    # Determine cycles that are defined as bursting throughout the whole cycle
-    df['is_burst'] = [frac == 1 for frac in burst_fracs]
-
-    df = _min_consecutive_cycles(df, n_cycles_min=n_cycles_min)
-    df['is_burst'] = df['is_burst'].astype(bool)
-
-    return df
--- bycycle/features.py
+++ bycycle/features.py
@@ -6,17 +6,16 @@ import numpy as np
 import pandas as pd
 
 from neurodsp.timefrequency import amp_by_time
+from neurodsp.burst import detect_bursts_dual_threshold
 
 from bycycle.cyclepoints import find_extrema, find_zerox
-from bycycle.burst import detect_bursts_cycles, detect_bursts_df_amp
 
 ###################################################################################################
 ###################################################################################################
 
-def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_method='cycles',
-                     burst_detection_kwargs=None, find_extrema_kwargs=None,
-                     hilbert_increase_n=False):
-    """Segment a recording into individual cycles and compute features for each cycle.
+def compute_shapes(sig, fs, f_range, center_extrema='peak',
+                   find_extrema_kwargs=None, hilbert_increase_n=False):
+    """Compute shapes parameters of each cycle, used for determining burst features.
 
     Parameters
     ----------
@@ -32,17 +31,9 @@ def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_me
         - 'peak' : cycles are defined trough-to-trough
         - 'trough' : cycles are defined peak-to-peak
 
-    burst_detection_method : {'cycles', 'amp'}
-        Method for detecting bursts.
-
-        - 'cycles': detect bursts based on the consistency of consecutive periods & amplitudes
-        - 'amp': detect bursts using an amplitude threshold
-
-    burst_detection_kwargs : dict, optional
-        Keyword arguments for function to find label cycles as in or not in an oscillation.
-    find_extrema_kwargs : dict, optional
+    find_extrema_kwargs : dict, optional, default: None
         Keyword arguments for function to find peaks an troughs (:func:`~.find_extrema`)
-        to change filter Parameters or boundary.By default, it sets the filter length to three
+        to change filter Parameters or boundary. By default, it sets the filter length to three
         cycles of the low cutoff frequency (``f_range[0]``).
     hilbert_increase_n : bool, optional, default: False
         Corresponding kwarg for :func:`~neurodsp.timefrequency.hilbert.amp_by_time`.
@@ -51,8 +42,9 @@ def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_me
 
     Returns
     -------
-    df : pandas.DataFrame
-        Dataframe containing features and identifiers for each cycle. Each row is one cycle.
+    df_shapes : pandas.DataFrame
+        Dataframe containing shape features that may be used to determine burst features using
+        :func:`~.compute_features`. Each row is one cycle.
         Columns (listed for peak-centered cycles):
 
         - ``sample_peak`` : sample of 'sig' at which the peak occurs
@@ -72,35 +64,20 @@ def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_me
         - ``volt_trough`` : voltage at the last trough
         - ``time_rdsym`` : fraction of cycle in the rise period
         - ``time_ptsym`` : fraction of cycle in the peak period
-        - ``band_amp`` : average analytic amplitude of the oscillation
-          computed using narrowband filtering and the Hilbert
-          transform. Filter length is 3 cycles of the low
-          cutoff frequency. Average taken across all time points
-          in the cycle.
-        - ``is_burst`` : True if the cycle is part of a detected oscillatory burst
-        - ``amp_fraction`` : normalized amplitude
-        - ``amp_consistency`` : difference in the rise and decay voltage within a cycle
-        - ``period_consistency`` : difference between a cycle’s period and the period of the
-          adjacent cycles
-        - ``monotonicity`` : fraction of instantaneous voltage changes between consecutive
-          samples that are positive during the rise phase and negative during the decay phase
+        - ``band_amp`` : average analytic amplitude of the oscillation computed using narrowband
+          filtering and the Hilbert transform. Filter length is 3 cycles of the low cutoff
+          frequency. Average taken across all time points in the cycle.
 
     Notes
     -----
     Peak vs trough centering
         - By default, the first extrema analyzed will be a peak, and the final one a trough.
         - In order to switch the preference, the signal is simply inverted and columns are renamed.
-        - Columns are slightly different depending on if ``center_extrema`` is set to 'peak' or 'trough'.
+        - Columns are slightly different depending on if ``center_extrema`` is set to 'peak' or
+          'trough'.
     """
 
     # Set defaults if user input is None
-    if burst_detection_kwargs is None:
-        burst_detection_kwargs = {}
-        warnings.warn('''
-            No burst detection parameters are provided. This is not recommended.
-            Check your data and choose appropriate parameters for "burst_detection_kwargs".
-            Default burst detection parameters are likely not well suited for the data.
-            ''')
     if find_extrema_kwargs is None:
         find_extrema_kwargs = {'filter_kwargs': {'n_cycles': 3}}
     else:
@@ -125,61 +102,52 @@ def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_me
     zerox_rise, zerox_decay = find_zerox(sig, ps, ts)
 
     # For each cycle, identify the sample of each extrema and zero-crossing
-    shape_features = {}
-    shape_features['sample_peak'] = ps[1:]
-    shape_features['sample_zerox_decay'] = zerox_decay[1:]
-    shape_features['sample_zerox_rise'] = zerox_rise
-    shape_features['sample_last_trough'] = ts[:-1]
-    shape_features['sample_next_trough'] = ts[1:]
+    shapes = {}
+    shapes['sample_peak'] = ps[1:]
+    shapes['sample_zerox_decay'] = zerox_decay[1:]
+    shapes['sample_zerox_rise'] = zerox_rise
+    shapes['sample_last_trough'] = ts[:-1]
+    shapes['sample_next_trough'] = ts[1:]
 
     # Compute duration of period
-    shape_features['period'] = shape_features['sample_next_trough'] - \
-        shape_features['sample_last_trough']
+    shapes['period'] = shapes['sample_next_trough'] - shapes['sample_last_trough']
 
     # Compute duration of peak
-    shape_features['time_peak'] = shape_features['sample_zerox_decay'] - \
-        shape_features['sample_zerox_rise']
+    shapes['time_peak'] = shapes['sample_zerox_decay'] - shapes['sample_zerox_rise']
 
     # Compute duration of last trough
-    shape_features['time_trough'] = zerox_rise - zerox_decay[:-1]
+    shapes['time_trough'] = zerox_rise - zerox_decay[:-1]
 
     # Determine extrema voltage
-    shape_features['volt_peak'] = sig[ps[1:]]
-    shape_features['volt_trough'] = sig[ts[:-1]]
+    shapes['volt_peak'] = sig[ps[1:]]
+    shapes['volt_trough'] = sig[ts[:-1]]
 
     # Determine rise and decay characteristics
-    shape_features['time_decay'] = (ts[1:] - ps[1:])
-    shape_features['time_rise'] = (ps[1:] - ts[:-1])
+    shapes['time_decay'] = (ts[1:] - ps[1:])
+    shapes['time_rise'] = (ps[1:] - ts[:-1])
 
-    shape_features['volt_decay'] = sig[ps[1:]] - sig[ts[1:]]
-    shape_features['volt_rise'] = sig[ps[1:]] - sig[ts[:-1]]
-    shape_features['volt_amp'] = (shape_features['volt_decay'] + shape_features['volt_rise']) / 2
+    shapes['volt_decay'] = sig[ps[1:]] - sig[ts[1:]]
+    shapes['volt_rise'] = sig[ps[1:]] - sig[ts[:-1]]
+    shapes['volt_amp'] = (shapes['volt_decay'] + shapes['volt_rise']) / 2
 
     # Compute rise-decay symmetry features
-    shape_features['time_rdsym'] = shape_features['time_rise'] / shape_features['period']
+    shapes['time_rdsym'] = shapes['time_rise'] / shapes['period']
 
     # Compute peak-trough symmetry features
-    shape_features['time_ptsym'] = shape_features['time_peak'] / \
-        (shape_features['time_peak'] + shape_features['time_trough'])
+    shapes['time_ptsym'] = shapes['time_peak'] / (shapes['time_peak'] + shapes['time_trough'])
 
     # Compute average oscillatory amplitude estimate during cycle
     amp = amp_by_time(sig, fs, f_range, hilbert_increase_n=hilbert_increase_n, n_cycles=3)
-    shape_features['band_amp'] = [np.mean(amp[ts[sig_idx]:ts[sig_idx + 1]]) for sig_idx in
-                                  range(len(shape_features['sample_peak']))]
+
+    shapes['band_amp'] = [np.mean(amp[ts[sig_idx]:ts[sig_idx + 1]]) for sig_idx in
+                          range(len(shapes['sample_peak']))]
 
     # Convert feature dictionary into a DataFrame
-    df = pd.DataFrame.from_dict(shape_features)
-
-    # Define whether or not each cycle is part of a burst
-    if burst_detection_method == 'cycles':
-        df = detect_bursts_cycles(df, sig, **burst_detection_kwargs)
-    elif burst_detection_method == 'amp':
-        df = detect_bursts_df_amp(df, sig, fs, f_range, **burst_detection_kwargs)
-    else:
-        raise ValueError('Invalid entry for "burst_detection_method"')
+    df_shapes = pd.DataFrame.from_dict(shapes)
 
     # Rename columns if they are actually trough-centered
     if center_extrema == 'trough':
+
         rename_dict = {'sample_peak': 'sample_trough',
                        'sample_zerox_decay': 'sample_zerox_rise',
                        'sample_zerox_rise': 'sample_zerox_decay',
@@ -193,12 +161,233 @@ def compute_features(sig, fs, f_range, center_extrema='peak', burst_detection_me
                        'time_decay': 'time_rise',
                        'volt_rise': 'volt_decay',
                        'volt_decay': 'volt_rise'}
-        df.rename(columns=rename_dict, inplace=True)
+        df_shapes.rename(columns=rename_dict, inplace=True)
 
         # Need to reverse symmetry measures
-        df['volt_peak'] = -df['volt_peak']
-        df['volt_trough'] = -df['volt_trough']
-        df['time_rdsym'] = 1 - df['time_rdsym']
-        df['time_ptsym'] = 1 - df['time_ptsym']
+        df_shapes['volt_peak'] = -df_shapes['volt_peak']
+        df_shapes['volt_trough'] = -df_shapes['volt_trough']
+        df_shapes['time_rdsym'] = 1 - df_shapes['time_rdsym']
+        df_shapes['time_ptsym'] = 1 - df_shapes['time_ptsym']
 
-    return df
+    return df_shapes
+
+
+def compute_features(df_shapes, sig, dual_threshold_kwargs=None):
+    """Compute burst features for each cycle.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+    sig : 1d array
+        Voltage time series used for determining monotonicity.
+    dual_threshold_kwargs : dict, optional, default: None
+        Additional arguments in :func:`~.compute_burst_fraction`.
+
+    Returns
+    -------
+    df_features : pandas.DataFrame
+        Dataframe containing burst features. Each row is one cycle.
+        Columns (listed for peak-centered cycles):
+
+    - ``amplitude_fraction`` : normalized amplitude
+    - ``amplitude_consistency`` : difference in the rise and decay voltage within a cycle
+    - ``period_consistency`` : difference between a cycle’s period and the period of the
+        adjacent cycles
+    - ``monotonicity`` : fraction of instantaneous voltage changes between consecutive
+        samples that are positive during the rise phase and negative during the decay phase
+
+    Notes
+    -----
+    Dual amplitude threshold burst detection will be used, rather than cycle feature consistency, if
+    ``dual_threshold_kwargs`` is not None.
+    """
+
+    # Compute burst features.
+    df_features = pd.DataFrame()
+
+    # Use feature consistency burst detection
+    if dual_threshold_kwargs is None:
+
+        # Custom feature functions may be inserted here as long as an array is return with a length
+        #   length equal to the number of cycles, or rows in df_shapes.
+        df_features['amplitude_fraction'] = compute_amplitude_fraction(df_shapes)
+        df_features['amplitude_consistency'] = compute_amplitude_consistency(df_shapes)
+        df_features['period_consistency'] = compute_period_consistency(df_shapes)
+        df_features['monotonicity'] = compute_monotonicity(df_shapes, sig)
+
+    # Use dual threshold burst detection
+    else:
+
+        df_features['burst_fraction'] = compute_burst_fraction(df_shapes, sig,
+                                                               **dual_threshold_kwargs)
+
+    return df_features
+
+
+def compute_amplitude_fraction(df_shapes):
+    """Compute the amplitude fraction of each cycle.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+
+
+    Returns
+    -------
+    amp_fract : 1d array
+        The amplitude fraction of each cycle.
+    """
+
+    return df_shapes['volt_amp'].rank()/len(df_shapes)
+
+
+def compute_amplitude_consistency(df_shapes):
+    """Compute ampltidue consistency for each cycle.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+
+
+    Returns
+    -------
+    amp_consist : 1d array
+        The amplitude consistency of each cycle.
+    """
+
+    # Compute amplitude consistency
+    cycles = len(df_shapes)
+    amplitude_consistency = np.ones(cycles) * np.nan
+    rises = df_shapes['volt_rise'].values
+    decays = df_shapes['volt_decay'].values
+
+    for cyc in range(1, cycles-1):
+
+        consist_current = np.min([rises[cyc], decays[cyc]]) / np.max([rises[cyc], decays[cyc]])
+
+        if 'sample_peak' in df_shapes.columns:
+            consist_last = np.min([rises[cyc], decays[cyc-1]]) / np.max([rises[cyc], decays[cyc-1]])
+            consist_next = np.min([rises[cyc+1], decays[cyc]]) / np.max([rises[cyc+1], decays[cyc]])
+
+        else:
+            consist_last = np.min([rises[cyc-1], decays[cyc]]) / np.max([rises[cyc-1], decays[cyc]])
+            consist_next = np.min([rises[cyc], decays[cyc+1]]) / np.max([rises[cyc], decays[cyc+1]])
+
+        amplitude_consistency[cyc] = np.min([consist_current, consist_next, consist_last])
+
+    return amplitude_consistency
+
+
+def compute_period_consistency(df_shapes):
+    """Compute the period consistency of each cycle.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+
+    Returns
+    -------
+    period_consistency : 1d array
+        The period consistency of each cycle.
+    """
+
+    # Compute period consistency
+    cycles = len(df_shapes)
+    period_consistency = np.ones(cycles) * np.nan
+    periods = df_shapes['period'].values
+
+    for cyc in range(1, cycles-1):
+
+        consist_last = np.min([periods[cyc], periods[cyc-1]]) / \
+            np.max([periods[cyc], periods[cyc-1]])
+        consist_next = np.min([periods[cyc+1], periods[cyc]]) / \
+            np.max([periods[cyc+1], periods[cyc]])
+
+        period_consistency[cyc] = np.min([consist_next, consist_last])
+
+    return period_consistency
+
+
+def compute_monotonicity(df_shapes, sig):
+    """Compute the monotonicity of each cycle.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+
+    Returns
+    -------
+    monotonicity : 1d array
+        The monotonicity of each cycle.
+    """
+
+    # Compute monotonicity
+    cycles = len(df_shapes)
+    monotonicity = np.ones(cycles) * np.nan
+
+    for idx, row in df_shapes.iterrows():
+
+        if 'sample_peak' in df_shapes.columns:
+            rise_period = sig[int(row['sample_last_trough']):int(row['sample_peak'])]
+            decay_period = sig[int(row['sample_peak']):int(row['sample_next_trough'])]
+
+        else:
+            decay_period = sig[int(row['sample_last_peak']):int(row['sample_trough'])]
+            rise_period = sig[int(row['sample_trough']):int(row['sample_next_peak'])]
+
+        decay_mono = np.mean(np.diff(decay_period) < 0)
+        rise_mono = np.mean(np.diff(rise_period) > 0)
+        monotonicity[idx] = np.mean([decay_mono, rise_mono])
+
+    return monotonicity
+
+
+def compute_burst_fraction(df_shapes, sig, fs, f_range, amp_threshes=(1, 2),
+                           n_cycles_min=3, filter_kwargs=None):
+    """ Compute the proportion of a cycle that is bursting.
+
+    Parameters
+    ----------
+    df_shapes : pandas DataFrame
+        Shape parameters for each cycle, determined using :func:`~.compute_shapes`.
+    sig : 1d array
+        Time series.
+    fs : float
+        Sampling rate, Hz.
+    f_range : tuple of (float, float)
+        Frequency range (Hz) for oscillator of interest.
+    amp_threshes : tuple (low, high), optional, default: (1, 2)
+        Threshold values for determining timing of bursts.
+        These values are in units of amplitude (or power, if specified) normalized to
+        the median amplitude (value 1).
+    n_cycles_min : int, optional, default: 3
+        Minimum number of cycles to be identified as truly oscillating needed in a row in
+        order for them to remain identified as truly oscillating.
+    filter_kwargs : dict, optional, default: None
+        Keyword arguments to :func:`~neurodsp.filt.filter.filter_signal`.
+
+    Returns
+    -------
+    burst_fraction : 1d array
+        The proportion of each cycle that is bursting.
+    """
+
+    filter_kwargs = {} if filter_kwargs is None else filter_kwargs
+
+    # Detect bursts using the dual amplitude threshold approach
+    is_burst = detect_bursts_dual_threshold(sig, fs, amp_threshes, f_range,
+                                            min_n_cycles=n_cycles_min, **filter_kwargs)
+
+    # Compute fraction of each cycle that's bursting
+    burst_fraction = []
+    for _, row in df_shapes.iterrows():
+        fraction_bursting = np.mean(is_burst[int(row['sample_last_trough']):
+                                             int(row['sample_next_trough'] + 1)])
+        burst_fraction.append(fraction_bursting)
+
+    return burst_fraction
