ed42ee86a4b0615da01c73f6074c4395c20ecffc
ryanhammonds <ryan.hammonds@utexas.edu>
2020-07-15 19:29:26 -0700

    new examples updated

--- examples/plot_mne_feature_distributions.py
+++ examples/plot_mne_feature_distributions.py
@@ -1,6 +1,6 @@
 """
-MNE interface cycle feature distributions
-=============================================
+MNE Interface Cycle Feature Distributions
+=========================================
 This example computes the distributions of bycycle features using MNE objects
 """
 
@@ -8,30 +8,30 @@ This example computes the distributions of bycycle features using MNE objects
 # Import Packages and Load Data
 # -----------------------------
 #
-# First let's import the packages we need. This example depends on mne as well
-# as the pactools simulator to make pac and a spurious pac function from the
-# pactools spurious pac example.
+# First let's import the packages we need. This example depends on mne.
 ####################################################################################################
 
-import mne
-from mne.datasets import sample
 import numpy as np
 import matplotlib.pyplot as plt
 
+from mne.io import read_raw_fif
+from mne.datasets import sample
+from mne import pick_channels
+
+from neurodsp.plts import plot_time_series
 from bycycle.features import compute_features
+from bycycle.plts import plot_feature_hist
 
 ###################################################################################################
 
-# frequencies of interest: the alpha band
+# Frequencies of interest: the alpha band
 f_alpha = (8, 15)
 
 # Get the data path for the MNE example data
 raw_fname = sample.data_path() + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 
 # Load the file of example MNE data
-raw = mne.io.read_raw_fif(raw_fname, preload=True, verbose=False)
-
-###################################################################################################
+raw = read_raw_fif(raw_fname, preload=True, verbose=False)
 
 # Select EEG channels from the dataset
 raw = raw.pick_types(meg=False, eeg=True, eog=False, exclude='bads')
@@ -42,18 +42,14 @@ fs = raw.info['sfreq']
 # filter to alpha
 raw = raw.filter(l_freq=None, h_freq=20.)
 
-###################################################################################################
-
-# Settings for exploring example channels of data (with colors to plot)
-chs = {'EEG 042': 'C0', 'EEG 043': 'C1', 'EEG 044': 'C2'}
+# Settings for exploring example channels of data
+chs = ['EEG 042', 'EEG 043', 'EEG 044']
 t_start = 20000
 t_stop = int(t_start + (10 * fs))
 
-###################################################################################################
-
 # Extract an example channels to explore
-sig, times = raw.get_data(mne.pick_channels(raw.ch_names, list(chs.keys())),
-                          start=t_start, stop=t_stop, return_times=True)
+sigs, times = raw.get_data(pick_channels(raw.ch_names, chs),
+                           start=t_start, stop=t_stop, return_times=True)
 
 ####################################################################################################
 #
@@ -66,13 +62,8 @@ sig, times = raw.get_data(mne.pick_channels(raw.ch_names, list(chs.keys())),
 
 ####################################################################################################
 
-# plot the signal
-fig, ax = plt.subplots(figsize=(10, 4))
-for ch, sig0 in zip(chs, sig):
-    ax.plot(times, sig0 * 1e6, color=chs[ch], label=ch)
-ax.set(title='EEG Signal', xlabel='Time (sec)', ylabel=r'$\mu V$')
-ax.legend()
-plt.show()
+# Plot the signal
+plot_time_series(times, [sig * 1e6 for sig in sigs], labels=chs, title='EEG Signal')
 
 ####################################################################################################
 # Compute cycle-by-cycle features
@@ -91,12 +82,13 @@ osc_kwargs = {'amplitude_fraction_threshold': 0.3,
               'monotonicity_threshold': 0.8,
               'n_cycles_min': 3}
 
+# Create a dictionary of cycle feature dataframes, corresponding to each channel
 dfs = dict()
-for ch, sig0 in zip(chs, sig):
-    # Cycle-by-cycle analysis
-    dfs[ch] = compute_features(sig0, fs, f_alpha, center_extrema='T',
-                               burst_detection_kwargs=osc_kwargs)
 
+for sig, ch in zip(sigs, chs):
+    # Cycle-by-cycle analysis
+    dfs[ch] = compute_features(sig, fs, f_alpha, center_extrema='trough',
+                                burst_detection_kwargs=osc_kwargs, return_samples=False)
 
 ####################################################################################################
 #
@@ -111,51 +103,23 @@ for ch, sig0 in zip(chs, sig):
 
 ####################################################################################################
 
-plt.figure(figsize=(5, 5))
-for ch, df in dfs.items():
-    plt.hist(df['volt_amp'] * 1e6, bins=np.arange(0, 40, 4),
-             color=chs[ch], alpha=.5, label=ch)
-plt.xticks(np.arange(0, 40, 4), size=12)
-plt.legend(fontsize=15)
-plt.yticks(size=12)
-plt.xlim((0, 40.5))
-plt.xlabel('Cycle amplitude (mV)', size=15)
-plt.ylabel('# cycles', size=15)
-plt.tight_layout()
-plt.show()
+fig, axes = plt.subplots(figsize=(15, 15), nrows=2, ncols=2)
 
-plt.figure(figsize=(5, 5))
 for ch, df in dfs.items():
-    plt.hist(df['period'] / fs * 1000, bins=np.arange(0, 250, 25),
-             color=chs[ch], alpha=.5)
-plt.xticks(size=12)
-plt.yticks(size=12)
-plt.xlim((0, 250))
-plt.xlabel('Cycle period (ms)', size=15)
-plt.ylabel('# cycles', size=15)
-plt.tight_layout()
-plt.show()
 
-bins = np.arange(0, 1, .1)
-plt.figure(figsize=(5, 5))
-for ch, df in dfs.items():
-    plt.hist(df['time_rdsym'], bins=bins, color=chs[ch], alpha=.5)
-plt.xticks(size=12)
-plt.yticks(size=12)
-plt.xlim((0, 1))
-plt.xlabel('Rise-decay asymmetry\n(fraction of cycle in rise period)', size=15)
-plt.ylabel('# cycles', size=15)
-plt.tight_layout()
-plt.show()
+    # Rescale amplitude and period features
+    df['volt_amp'] = df['volt_amp'] * 1e6
+    df['period'] = df['period'] / fs * 1000
 
-plt.figure(figsize=(5, 5))
-for ch, df in dfs.items():
-    plt.hist(df['time_ptsym'], bins=bins, color=chs[ch], alpha=.5)
-plt.xticks(size=12)
-plt.yticks(size=12)
-plt.xlim((0, 1))
-plt.xlabel('Peak-trough asymmetry\n(fraction of cycle in peak period)',
-           size=15)
-plt.ylabel('# cycles', size=15)
-plt.tight_layout()
-plt.show()
+    # Plot feature histograms
+    plot_feature_hist(df, 'volt_amp', only_bursts=False, ax=axes[0][0], label=ch,
+                      xlabel='Cycle amplitude (mV)', bins=np.arange(0, 40, 4))
+
+    plot_feature_hist(df, 'period', only_bursts=False, ax=axes[0][1], label=ch,
+                      xlabel='Cycle period (ms)', bins=np.arange(0, 250, 25))
+
+    plot_feature_hist(df, 'time_rdsym', only_bursts=False, ax=axes[1][0], label=ch,
+                      xlabel='Rise-decay asymmetry', bins=np.arange(0, 1, .1))
+
+    plot_feature_hist(df, 'time_ptsym', only_bursts=False, ax=axes[1][1], label=ch,
+                      xlabel='Peak-trough asymmetry', bins=np.arange(0, 1, .1))
