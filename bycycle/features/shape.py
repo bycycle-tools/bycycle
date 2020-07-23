f31f24ca8368b96dd6ce41a89ea7340f3cc59e7a
ryanhammonds <ryan.hammonds@utexas.edu>
2020-07-23 14:45:23 -0700

    increased modularity in shape feature funcs

--- bycycle/features/shape.py
+++ bycycle/features/shape.py
@@ -30,7 +30,7 @@ def compute_shape_features(sig, fs, f_range, center_extrema='peak', find_extrema
 
     find_extrema_kwargs : dict, optional, default: None
         Keyword arguments for function to find peaks an troughs (:func:`~.find_extrema`)
-        to change filter Parameters or boundary. By default, it sets the filter length to three
+        to change filter parameters or boundary. By default, it sets the filter length to three
         cycles of the low cutoff frequency (``f_range[0]``).
     hilbert_increase_n : bool, optional, default: False
         Corresponding kwarg for :func:`~neurodsp.timefrequency.hilbert.amp_by_time`.
@@ -43,7 +43,7 @@ def compute_shape_features(sig, fs, f_range, center_extrema='peak', find_extrema
 
     Returns
     -------
-    df_shapes : pandas.DataFrame
+    df_shape_features : pandas.DataFrame
         Dataframe containing cycle shape features. Each row is one cycle. Columns:
 
         - ``period`` : period of the cycle
@@ -99,55 +99,46 @@ def compute_shape_features(sig, fs, f_range, center_extrema='peak', find_extrema
     else:
         raise ValueError('Parameter "center_extrema" must be either "peak" or "trough"')
 
-    # Find extrema and zero-crossings locations in the signal
-    peaks, troughs = find_extrema(sig, fs, f_range, **find_extrema_kwargs)
-    rises, decays = find_zerox(sig, peaks, troughs)
-
     # For each cycle, identify the sample of each extrema and zero-crossing
-    df_samples = compute_samples(peaks, troughs, decays, rises)
-
-    # Initialize dictionary for shape features
-    shape_features = {}
+    df_samples = compute_samples(sig, fs, f_range, **find_extrema_kwargs)
 
     # Compute durations of period, peaks, and troughs
-    shape_features['period'] = df_samples['sample_next_trough'] - df_samples['sample_last_trough']
-    shape_features['time_peak'] = df_samples['sample_zerox_decay'] - df_samples['sample_zerox_rise']
-    shape_features['time_trough'] = rises - decays[:-1]
+    period, time_peak, time_trough = compute_durations(df_samples)
 
-    # Determine extrema voltage
-    shape_features['volt_peak'] = sig[peaks[1:]]
-    shape_features['volt_trough'] = sig[troughs[:-1]]
+    # Compute extrema voltage
+    volt_peak, volt_trough = compute_extrema_voltage(df_samples, sig)
 
-    # Determine rise and decay characteristics
-    shape_features['time_decay'] = (troughs[1:] - peaks[1:])
-    shape_features['time_rise'] = (peaks[1:] - troughs[:-1])
-
-    shape_features['volt_decay'] = sig[peaks[1:]] - sig[troughs[1:]]
-    shape_features['volt_rise'] = sig[peaks[1:]] - sig[troughs[:-1]]
-    shape_features['volt_amp'] = (shape_features['volt_decay'] + shape_features['volt_rise']) / 2
-
-    # Compute rise-decay symmetry features
-    shape_features['time_rdsym'] = shape_features['time_rise'] / shape_features['period']
-
-    # Compute peak-trough symmetry features
-    shape_features['time_ptsym'] = shape_features['time_peak'] / \
-            (shape_features['time_peak'] + shape_features['time_trough'])
+    # Compute rise-decay and peak-trough features and characteristics
+    sym_features = compute_symmetry(df_samples, sig, period=period,
+                                    time_peak=time_peak, time_trough=time_trough)
 
     # Compute average oscillatory amplitude estimate during cycle
-    amp = amp_by_time(sig, fs, f_range, n_cycles=n_cycles, hilbert_increase_n=hilbert_increase_n)
-    shape_features['band_amp'] = [np.mean(amp[troughs[sig_idx]:troughs[sig_idx + 1]]) for sig_idx in
-                                  range(len(df_samples['sample_peak']))]
+    band_amp = compute_band_amp(df_samples, sig, fs, f_range, hilbert_increase_n=False, n_cycles=3)
 
-    # Convert feature dictionary into a DataFrame
-    df_features = pd.DataFrame.from_dict(shape_features)
+    # Organize shape features into a dataframe
+    shape_features = {}
+    shape_features['period'] = period
+    shape_features['time_peak'] = time_peak
+    shape_features['time_trough'] = time_trough
+    shape_features['volt_peak'] = volt_peak
+    shape_features['volt_trough'] = volt_trough
+    shape_features['time_decay'] = sym_features['time_decay']
+    shape_features['time_rise'] = sym_features['time_rise']
+    shape_features['volt_decay'] = sym_features['volt_decay']
+    shape_features['volt_rise'] = sym_features['volt_rise']
+    shape_features['volt_amp'] = sym_features['volt_amp']
+    shape_features['time_rdsym'] = sym_features['time_rdsym']
+    shape_features['time_ptsym'] = sym_features['time_ptsym']
+    shape_features['band_amp'] = band_amp
+    df_shape_features = pd.DataFrame.from_dict(shape_features)
 
     # Rename the dataframe if trough centered
-    df_features, df_samples = _rename_df(center_extrema, df_samples, df_features)
+    df_shape_features, df_samples = _rename_df(center_extrema, df_samples, df_shape_features)
 
     if return_samples:
-        return df_features, df_samples
+        return df_shape_features, df_samples
 
-    return df_features
+    return df_shape_features
 
 
 def _rename_df(center_extrema, df_samples, df_features):
@@ -183,26 +174,178 @@ def _rename_df(center_extrema, df_samples, df_features):
     return df_features, df_samples
 
 
-def compute_samples(peaks, troughs, decays, rises):
+def compute_durations(df_samples):
+    """Compute the time durations of periods, peaks, and troughs.
+
+    Parameters
+    ---------
+    df_samples : pandas DataFrame
+        Dataframe containing sample indices of cyclepoints.
+
+    Returns
+    -------
+    period : 1d array
+        The period of each cycle.
+    time_peak : 1d array
+        Time between peak and next trough.
+    time_trough : 1d array
+        Time between peak and previous trough
+    """
+
+    period = df_samples['sample_next_trough'] - df_samples['sample_last_trough']
+    time_peak = df_samples['sample_zerox_decay'] - df_samples['sample_zerox_rise']
+    time_trough = df_samples['sample_zerox_rise'] - df_samples['sample_last_zerox_decay']
+
+    return period, time_peak, time_trough
+
+
+def compute_extrema_voltage(df_samples, sig):
+    """Compute the voltage of peaks and troughs.
+
+    Parameters
+    ---------
+    df_samples : pandas DataFrame
+        Dataframe containing sample indices of cyclepoints.
+    sig : 1d array
+        Time series.
+
+    Returns
+    -------
+    volt_peak : 1d array
+        Voltage at the peak.
+    volt_trough : 1d array
+        Voltage at the last trough.
+    """
+
+    volt_peak = sig[df_samples['sample_peak']]
+    volt_trough = sig[df_samples['sample_last_trough']]
+
+    return volt_peak, volt_trough
+
+
+def compute_symmetry(df_samples, sig, period=None, time_peak=None, time_trough=None):
+    """Compute rise-decay and peak-trough characteristics.
+
+    Parameters
+    ---------
+    df_samples : pandas DataFrame
+        Dataframe containing sample indices of cyclepoints.
+    sig : 1d array
+        Time series.
+    period : 1d array, optional, default: None
+        The period of each cycle.
+    time_peak : 1d array, optional, default: None
+        Time between peak and next trough.
+    time_trough : 1d array, optional, default: None
+        Time between peak and previous trough
+
+    Returns
+    -------
+    sym_features : dict
+        Contains 1d arrays of symmetry features. Keys include:
+
+        - time_decay : Time between peak and next trough.
+        - time_rise : Time between peak and previous trough.
+        - volt_decay : Voltage change between peak and next trough.
+        - volt_rise : Voltage change between peak and previous trough.
+        - volt_amp : Average of rise and decay voltage.
+        - time_rdsym : Fraction of cycle in the rise period.
+        - time_ptsym : Fraction of cycle in the peak period.
+
+    """
+
+    # Determine rise and decay characteristics
+    sym_features = {}
+
+    time_decay = df_samples['sample_next_trough'] - df_samples['sample_peak']
+    time_rise = df_samples['sample_peak'] - df_samples['sample_last_trough']
+    volt_decay = sig[df_samples['sample_peak']] - sig[df_samples['sample_next_trough']]
+    volt_rise = sig[df_samples['sample_peak']] - sig[df_samples['sample_last_trough']]
+    volt_amp = (volt_decay + volt_rise) / 2
+
+    # Compute rise-decay symmetry features
+    if period is None or time_peak is None or time_trough is None:
+        period, time_peak, time_trough = compute_durations(df_samples)
+
+    time_rdsym = time_rise / period
+
+    # Compute peak-trough symmetry features
+    time_ptsym = time_peak / (time_peak + time_trough)
+
+    sym_features['time_decay'] = time_decay
+    sym_features['time_rise'] = time_rise
+    sym_features['volt_decay'] = volt_decay
+    sym_features['volt_rise'] = volt_rise
+    sym_features['volt_amp'] = volt_amp
+    sym_features['time_rdsym'] = time_rdsym
+    sym_features['time_ptsym'] = time_ptsym
+
+    return sym_features
+
+
+def compute_band_amp(df_samples, sig, fs, f_range, hilbert_increase_n=False, n_cycles=3):
+    """Compute the average amplitude of each oscillation.
+
+    Parameters
+    ----------
+    sig : 1d array
+        Time series.
+    fs : float
+        Sampling rate, in Hz.
+    f_range : tuple of (float, float)
+        Frequency range for narrowband signal of interest (Hz).
+    hilbert_increase_n : bool, optional, default: False
+        Corresponding kwarg for :func:`~neurodsp.timefrequency.hilbert.amp_by_time`.
+        If true, this zero-pads the signal when computing the Fourier transform, which can be
+        necessary for computing it in a reasonable amount of time.
+    n_cycles : int, optional, default: 3
+        Length of filter, in number of cycles, at the lower cutoff frequency.
+
+    Returns
+    -------
+    band_amp : 1d array
+        Average analytic amplitude of the oscillation computed using narrowband filtering and the
+        Hilbert transform. Filter length is 3 cycles of the low cutoff frequency. Average taken
+        across all time points in the cycle.
+    """
+
+    amp = amp_by_time(sig, fs, f_range, n_cycles=n_cycles, hilbert_increase_n=hilbert_increase_n)
+
+    troughs = np.append(df_samples['sample_last_trough'].values[0],
+                        df_samples['sample_next_trough'].values)
+
+    band_amp = [np.mean(amp[troughs[sig_idx]:troughs[sig_idx + 1]]) for
+                sig_idx in range(len(df_samples['sample_peak']))]
+
+    return band_amp
+
+
+def compute_samples(sig, fs, f_range, **find_extrema_kwargs):
     """Compute sample indices for cyclepoints.
 
     Parameters
     ----------
-    peaks : 1d array
-        Signal indices of oscillatory peaks.
-    troughs : 1d array
-        Signal indices of oscillatory troughs.
-    rises : 1d array, optional
-        Signal indices of oscillatory rising zero-crossings.
-    decays : 1d array, optional
-        Signal indices of oscillatory decaying zero-crossings.
+    sig : 1d array
+        Time series.
+    fs : float
+        Sampling rate, in Hz.
+    f_range : tuple of (float, float)
+        Frequency range, in Hz, to narrowband filter the signal, used to find zero-crossings.
+    find_extrema_kwargs : dict, optional, default: None
+        Keyword arguments for function to find peaks an troughs (:func:`~.find_extrema`)
+        to change filter parameters or boundary. By default, it sets the filter length to three
+        cycles of the low cutoff frequency (``f_range[0]``).
 
     Returns
     -------
-    df_samples : pandas.DataFrame, optional, default: False
+    df_samples : pandas DataFrame, optional, default: False
         Dataframe containing sample indices of cyclepoints.
         Columns (listed for peak-centered cycles):
 
+        - ``peaks`` : signal indices of oscillatory peaks
+        - ``troughs`` :  signal indices of oscillatory troughs
+        - ``rises`` : signal indices of oscillatory rising zero-crossings
+        - ``decays`` : signal indices of oscillatory decaying zero-crossings
         - ``sample_peak`` : sample of 'sig' at which the peak occurs
         - ``sample_zerox_decay`` : sample of the decaying zero-crossing
         - ``sample_zerox_rise`` : sample of the rising zero-crossing
@@ -211,9 +354,14 @@ def compute_samples(peaks, troughs, decays, rises):
 
     """
 
+    # Find extrema and zero-crossings locations in the signal
+    peaks, troughs = find_extrema(sig, fs, f_range, **find_extrema_kwargs)
+    rises, decays = find_zerox(sig, peaks, troughs)
+
     # For each cycle, identify the sample of each extrema and zero-crossing
     samples = {}
     samples['sample_peak'] = peaks[1:]
+    samples['sample_last_zerox_decay'] = decays[:-1]
     samples['sample_zerox_decay'] = decays[1:]
     samples['sample_zerox_rise'] = rises
     samples['sample_last_trough'] = troughs[:-1]
--- bycycle/tests/test_features_shape.py
+++ bycycle/tests/test_features_shape.py
@@ -5,7 +5,8 @@ import pytest
 import numpy as np
 
 from bycycle.cyclepoints import find_extrema, find_zerox
-from bycycle.features import compute_shape_features, compute_samples
+from bycycle.features.shape import (compute_shape_features, compute_samples, compute_durations,
+                                    compute_extrema_voltage, compute_symmetry, compute_band_amp)
 
 ###################################################################################################
 ###################################################################################################
@@ -84,10 +85,66 @@ def test_compute_samples(sim_args):
     peaks, troughs = find_extrema(sig, fs, f_range)
     rises, decays = find_zerox(sig, peaks, troughs)
 
-    df_samples = compute_samples(peaks, troughs, decays, rises)
+    df_samples = compute_samples(sig, fs, f_range)
 
     assert (df_samples['sample_peak'] == peaks[1:]).all()
     assert (df_samples['sample_zerox_decay'] == decays[1:]).all()
     assert (df_samples['sample_zerox_rise'] == rises).all()
     assert (df_samples['sample_last_trough'] == troughs[:-1]).all()
     assert (df_samples['sample_next_trough'] == troughs[1:]).all()
+
+
+def test_compute_durations(sim_args):
+
+    df_samples = sim_args['df_samples']
+    period, time_peak, time_trough = compute_durations(df_samples)
+
+    assert ((time_trough + time_peak) == period).all()
+
+
+def test_compute_extrema_voltage(sim_args):
+
+    df_samples = sim_args['df_samples']
+    sig = sim_args['sig']
+
+    volt_peaks, volt_troughs = compute_extrema_voltage(df_samples, sig)
+
+    for idx, volt_peak in enumerate(volt_peaks):
+        assert volt_peak > volt_troughs[idx]
+
+
+@pytest.mark.parametrize("recompute", [True, False])
+def test_compute_symmetry(sim_args, recompute):
+
+    df_samples = sim_args['df_samples']
+    sig = sim_args['sig']
+
+    if recompute:
+        sym_features = compute_symmetry(df_samples, sig)
+    else:
+        period, time_peak, time_trough = compute_durations(df_samples)
+        sym_features = compute_symmetry(df_samples, sig, period=period,
+                                        time_peak=time_peak, time_trough=time_trough)
+
+    # This is the case for only simulated sine waves
+    assert (sym_features['time_decay'] == sym_features['time_rise']).all()
+    assert (sym_features['volt_decay'] == sym_features['volt_rise']).all()
+
+    assert (sym_features['volt_amp'] == \
+        (sym_features['volt_rise'] + sym_features['volt_decay'])/2).all()
+
+    np.testing.assert_almost_equal(sym_features['time_rdsym'].values,
+                                   sym_features['time_ptsym'].values, decimal=1)
+
+
+def test_compute_band_amp(sim_args):
+
+    df_samples = sim_args['df_samples']
+    sig = sim_args['sig']
+    fs = sim_args['fs']
+    f_range = sim_args['f_range']
+
+    band_amp = compute_band_amp(df_samples, sig, fs, f_range)
+
+    for amp in band_amp[:-1]:
+        np.testing.assert_allclose(amp, band_amp[0], rtol=1e-3)
