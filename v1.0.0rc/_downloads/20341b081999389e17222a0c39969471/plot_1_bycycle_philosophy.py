ad0b631dc06bf097ed56c116c86774b85ede8428
ryanhammonds <ryan.hammonds@utexas.edu>
2020-10-16 13:53:41 -0700

    Make install

---
+++ v1.0.0rc/_downloads/20341b081999389e17222a0c39969471/plot_1_bycycle_philosophy.py
@@ -0,0 +1,155 @@
+"""
+1. Cycle-by-cycle philosophy
+============================
+
+Neural signals, like the example shown below, are analyzed in order to extract information about
+brain activity. Basically, we process these signals in order to extract features that will hopefully
+correlate with a behavior, pathology, or something else.
+
+As the most prominent feature of these signals tends to be the oscillations in them, spectral
+analysis is often applied in order to characterize these rhythms in terms of their frequency, power,
+and phase.
+
+The conventional approach to analyzing these properties as a function of time is to only study a
+narrowband signal by applying a wavelet transform or bandpass filtering followed by the Hilbert
+transform. The latter is demonstrated below.
+
+"""
+
+###################################################################################################
+#
+# Conventional analysis of amplitude and phase: Hilbert Transform
+# ---------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from neurodsp.sim import sim_combined
+from neurodsp.utils import create_times
+from neurodsp.filt import filter_signal
+from neurodsp.timefrequency import amp_by_time, phase_by_time
+from neurodsp.plts import plot_time_series, plot_instantaneous_measure
+
+
+# Simulation settings
+n_seconds = 10
+fs = 1000
+components = {'sim_bursty_oscillation': {'freq': 10, 'enter_burst': .1, 'leave_burst': .1,
+                                         'cycle': 'asine', 'rdsym': 0.3},
+              'sim_powerlaw': {'f_range': (2, None)}}
+sig = sim_combined(n_seconds, fs, components=components, component_variances=(2, 1))
+
+# Filter settings
+f_alpha = (8, 12)
+n_seconds_filter = .5
+
+# Compute amplitude and phase
+sig_filt = filter_signal(sig, fs, 'bandpass', f_alpha, n_seconds=n_seconds_filter)
+theta_amp = amp_by_time(sig, fs, f_alpha, n_seconds=n_seconds_filter)
+theta_phase = phase_by_time(sig, fs, f_alpha, n_seconds=n_seconds_filter)
+
+# Plot signal
+times = create_times(n_seconds, fs)
+xlim = (2, 6)
+tidx = np.logical_and(times >= xlim[0], times < xlim[1])
+
+fig, axes = plt.subplots(figsize=(15, 9), nrows=3)
+
+# Plot the raw signal
+plot_time_series(times[tidx], sig[tidx], ax=axes[0], ylabel='Voltage (mV)',
+                 xlabel='', lw=2, labels='raw signal')
+
+# Plot the filtered signal and oscillation amplitude
+plot_instantaneous_measure(times[tidx], [sig_filt[tidx], theta_amp[tidx]],
+                           ax=axes[1], measure='amplitude', lw=2, xlabel='',
+                           labels=['filtered signal', 'amplitude'])
+
+# Plot the phase
+plot_instantaneous_measure(times[tidx], theta_phase[tidx], ax=axes[2], colors='r',
+                           measure='phase', lw=2, xlabel='Time (s)')
+
+####################################################################################################
+#
+# This conventional analysis has some advantages and disadvantages. As for advantages:
+#
+# - Quick calculation
+# - Neat mathematical theory
+# - Results largely make sense
+# - Defined at every point in time.
+#
+# Because of this last property, these traces have come to be known as "instantaneous amplitude"
+# and "instantaneous phase." And they seem to make a lot of sense, when looking at the raw signal.
+#
+# However, there are some key disadvantages to this analysis that stem from its sine wave basis.
+#
+# 1. Being defined at every point in time gives the illusion that the phase and amplitude estimates
+#    are valid at all points in time. However, the amplitude and phase estimates are pretty garbage
+#    when there's no oscillation going on (the latter half of the time series above). The "amplitude"
+#    and "phase" values are meaningless when no oscillation is actually present. Rather, they are
+#    influenced by the other aspects of the signal, such as transients. For this reason, these measures
+#    are flaws, and burst detection is very important to help alleviate this issue.
+# 2. This analysis does not capture a potentially important aspect of the data, in that the
+#    oscillatory cycles tend to have short rises and longer decays. This is partly because the signal
+#    is filtered in a narrow frequency band (using a sine wave basis) that cannot accurately
+#    reconstruct nonsinusoidal waveforms. Furthermore, this nonsinusoidal feature will unintuitively
+#    bias amplitude and phase estimates (though perhaps negligibly). Furthermore, there are no apparent
+#    tools for extracting nonsinusoidal properties using conventional techniques.
+#
+#
+# Note that different hyperparameter choices for filters can lead to significant differences in results
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# When effect sizes are small, these hyperparameter choices may make a crucial difference.
+#
+
+# Different hyperparameter choices - filter length and center frequency and bandwidth
+f_alphas = [(6, 14), (8, 12), (9, 13)]
+n_seconds = [.4, .75, 1.2]
+
+amps = []
+phases = []
+
+for f_alpha in f_alphas:
+
+    for n_second_filter in n_seconds:
+
+        amp = amp_by_time(sig, fs, f_alpha, n_seconds=n_second_filter)
+        phase = phase_by_time(sig, fs, f_alpha, n_seconds=n_second_filter)
+
+        amps.append(amp)
+        phases.append(phase)
+
+fig, axes = plt.subplots(figsize=(15, 6), nrows=2)
+
+plot_instantaneous_measure(times, amps, ax=axes[0], xlim=(2,6),
+                           measure='amplitude', lw=2, xlabel='')
+
+plot_instantaneous_measure(times, phases, ax=axes[1], xlim=(2,6),
+                           measure='phase',lw=2)
+
+####################################################################################################
+#
+# Cycle-by-cycle approach
+# =======================
+#
+# The main issues in the conventional approach are because the measurements of amplitude and phase
+# are very indirect, using certain transforms in the frequency domain defined by sine waves.
+# Therefore, we developed an alternative approach that analyzes oscillatory properties more directly
+# by staying in the time domain. Arguably, it is best to analyze these signals in the time domain
+# because this is the domain in which they are generated (the brain does not generate sums of
+# independent sine waves).
+#
+# The benefits of this alternative approach may include:
+#
+# - More direct measurements of amplitude and frequency may be more accurate (see Figures 5 and 6 \
+#   in the associated preprint).
+# - Characterization of waveform shape, in addition to amplitude and phase and frequency.
+# - Explicit definitions of which portions of the signal are suitable for analysis (in oscillatory \
+#   bursts) or not (no oscillation present).
+# - It is important to note that this approach also has some key disadvantages. First, it is not \
+#   widely adopted like the conventional techniques. Second, it requires more hyperparameter \
+#   choosing and potentialyy more quality control compared to conventional techniques. I emphasize \
+#   how important it is to visualize the cycle-by-cycle characterization and burst detection to \
+#   assure that the metrics match the intuition. However, this is not commonly expected or \
+#   performed using conventional techniques.
+#
---
+++ v1.0.0rc/_downloads/598c26724a41bf225a3d7a900f1bb5b5/plot_theta_feature_distributions.py
@@ -0,0 +1,143 @@
+"""
+Theta oscillation cycle feature distributions
+=============================================
+This tutorial computes the distributions of cycle features for two recordings
+"""
+
+####################################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from neurodsp.filt import filter_signal
+from neurodsp.plts import plot_time_series
+
+from bycycle.group import compute_features_2d
+from bycycle.plts.features import plot_feature_hist
+
+####################################################################################################
+# Load and preprocess data
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+####################################################################################################
+
+# Load data
+ca1_raw = np.load('data/ca1.npy')
+ec3_raw = np.load('data/ec3.npy')
+fs = 1250
+f_theta = (4, 10)
+
+####################################################################################################
+
+# Only keep 60 seconds of data
+n_seconds = 60
+ca1_raw = ca1_raw[:int(n_seconds*fs)]
+ec3_raw = ec3_raw[:int(n_seconds*fs)]
+
+####################################################################################################
+
+# Apply a lowpass filter at 25Hz
+fc = 25
+filter_seconds = .5
+
+ca1 = filter_signal(ca1_raw, fs, 'lowpass', fc, n_seconds=filter_seconds,
+                    remove_edges=False)
+ec3 = filter_signal(ec3_raw, fs, 'lowpass', fc, n_seconds=filter_seconds,
+                    remove_edges=False)
+
+####################################################################################################
+# Compute cycle-by-cycle features
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+####################################################################################################
+
+# Set parameters for defining oscillatory bursts
+threshold_kwargs = {'amp_fraction_threshold': 0,
+                    'amp_consistency_threshold': .6,
+                    'period_consistency_threshold': .75,
+                    'monotonicity_threshold': .8,
+                    'min_n_cycles': 3}
+
+# Cycle-by-cycle analysis
+sigs = np.array([ca1, ec3])
+
+compute_features_kwargs = {'center_extrema': 'trough', 'threshold_kwargs': threshold_kwargs}
+
+df_features = compute_features_2d(sigs, fs, f_theta, return_samples=False,
+                                  compute_features_kwargs=compute_features_kwargs)
+
+df_ca1, df_ec3 = df_features[0], df_features[1]
+
+# Limit analysis only to oscillatory bursts
+df_ca1_cycles = df_ca1[df_ca1['is_burst']]
+df_ec3_cycles = df_ec3[df_ec3['is_burst']]
+
+####################################################################################################
+#
+# Plot time series for each recording
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+####################################################################################################
+
+# Choose samples to plot
+samplims = (10000, 12000)
+ca1_plt = ca1_raw[samplims[0]:samplims[1]]/1000
+ec3_plt = ec3_raw[samplims[0]:samplims[1]]/1000
+times = np.arange(0, len(ca1_plt)/fs, 1/fs)
+
+fig, axes = plt.subplots(figsize=(15, 6), nrows=2)
+
+plot_time_series(times, ca1_plt, ax=axes[0], xlim=(0, 1.6), ylim=(-2.4, 2.4),
+                 xlabel="Time (s)", ylabel="CA1 Voltage (mV)")
+
+plot_time_series(times, ec3_plt, ax=axes[1], colors='r', xlim=(0, 1.6),
+                 ylim=(-2.4, 2.4), xlabel="Time (s)", ylabel="EC3 Voltage (mV)")
+
+####################################################################################################
+#
+# Plot feature distributions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+####################################################################################################
+
+fig, axes = plt.subplots(figsize=(15, 15), nrows=2, ncols=2)
+
+# Plot cycle amplitude
+cycles_ca1 = df_ca1_cycles['volt_amp']/1000
+cycles_ec3 = df_ec3_cycles['volt_amp']/1000
+
+plot_feature_hist(cycles_ca1, 'volt_amp', ax=axes[0][0], xlabel='Cycle amplitude (mV)',
+                  xlim=(0, 4.5), color='k', bins=np.arange(0, 8, .1))
+
+plot_feature_hist(cycles_ec3, 'volt_amp', ax=axes[0][0], xlabel='Cycle amplitude (mV)',
+                  xlim=(0, 4.5), color='r', bins=np.arange(0, 8, .1))
+
+axes[0][0].legend(['CA1', 'EC3'], fontsize=15)
+
+# Plot cycle period
+periods_ca1 = df_ca1_cycles['period'] / fs * 1000
+periods_ec3 = df_ec3_cycles['period'] / fs * 1000
+
+plot_feature_hist(periods_ca1, 'period', ax=axes[0][1], xlabel='Cycle period (ms)',
+                  xlim=(0, 250), color='k', bins=np.arange(0, 250, 5))
+
+plot_feature_hist(periods_ec3, 'volt_amp', ax=axes[0][1], xlabel='Cycle period (ms)',
+                  xlim=(0, 250), color='r', bins=np.arange(0, 250, 5))
+
+# Plot rise/decay symmetry
+plot_feature_hist(df_ca1_cycles, 'time_rdsym', ax=axes[1][0], xlim=(0, 1), color='k',
+                  xlabel='Rise-decay asymmetry\n(fraction of cycle in rise period)',
+                  bins=np.arange(0, 1, .02))
+
+plot_feature_hist(df_ec3_cycles, 'time_rdsym', ax=axes[1][0], xlim=(0, 1), color='r',
+                  xlabel='Rise-decay asymmetry\n(fraction of cycle in rise period)',
+                  bins=np.arange(0, 1, .02))
+
+# Plot peak/trough symmetry
+plot_feature_hist(df_ca1_cycles, 'time_ptsym', ax=axes[1][1], color='k',
+                  xlabel='Peak-trough asymmetry\n(fraction of cycle in peak period)',
+                  bins=np.arange(0, 1, .02))
+
+plot_feature_hist(df_ec3_cycles, 'time_ptsym', ax=axes[1][1], color='r',
+                  xlabel='Peak-trough asymmetry\n(fraction of cycle in peak period)',
+                  bins=np.arange(0, 1, .02))
