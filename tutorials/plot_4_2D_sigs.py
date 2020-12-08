b8da68c70b88d3a7aa69e08ea66daaa2b2518405
ryanhammonds <ryan.hammonds@utexas.edu>
2020-12-08 15:07:40 -0800

    review updates for tutorials/examples

--- tutorials/plot_4_2D_sigs.py
+++ tutorials/plot_4_2D_sigs.py
@@ -8,8 +8,8 @@ Bycycle supports computing the features of 2D signals using :func:`~.compute_fea
 Signals may be organized in a different ways, including (n_epochs, n_timepoints) or
 (n_channels, n_timepoints). The difference between these organizataions is that continuity is
 preserved across epochs, but not across channels. The ``axis`` argument is used to specificy either
-continuous (``axis=1``) and non-continuous (``axis=None``) recordings. This argument functions
-identically to numpy functions, such as np.mean or np.sum.
+continuous (``axis=None``) and non-continuous (``axis=0``) recordings.
+
 """
 
 ####################################################################################################
@@ -26,23 +26,26 @@ from bycycle.plts import plot_feature_categorical
 # The ``axis`` Argument
 # ---------------------
 #
-# Here, we will examine an analogous ``np.sum`` example of the axis argument before moving
-# onto specific bycycle examples. The axis argument be may specified as:
+# Here, we will show how the axis arguments works. The axis argument be may specified as:
 #
-# - ``axis=1`` : Operates across each row/signal in an array independently (i.e. for each
-#   channel in (n_channels, n_timepoints)).
-# - ``axis=None`` : Operates on flattened rows/signals (i.e. across flatten epochs
+# - ``axis=1`` : Iterates over each row/signal in an array independently (i.e. for each channel
+#   in (n_channels, n_timepoints)).
+# - ``axis=None`` : Flattens rows/signals prior to computing features (i.e. across flatten epochs
 #   in (n_epochs, n_timepoints)).
 #
 
 ####################################################################################################
 
-# Analagous to (n_channels, n_timepoints) or (n_epochs, n_timepoints)
-arr = np.ones((2, 3))
+arr = np.ones((3, 4))
 
-print(arr, "\n")
-print("axis=1    (channels): ", np.sum(arr, axis=1))
-print("axis=None (epochs)  : ", np.sum(arr, axis=None))
+dim0_len = np.shape(arr)[0]
+
+print("axis=0")
+for dim0 in range(dim0_len):
+    print(np.shape(arr[dim0]))
+
+print("\naxis=None")
+print(np.shape(arr.flatten()))
 
 ####################################################################################################
 #
@@ -97,8 +100,8 @@ thresholds = dict(amp_fraction_threshold=0., amp_consistency_threshold=.5,
 
 compute_kwargs = {'burst_method': 'cycles', 'threshold_kwargs': thresholds}
 
-df_features = compute_features_2d(sigs, fs, f_range, axis=None,
-                                  compute_features_kwargs=compute_kwargs)
+df_features = compute_features_2d(sigs, fs, f_range, compute_features_kwargs=compute_kwargs,
+                                  axis=None)
 
 ####################################################################################################
 
@@ -122,9 +125,10 @@ plot_feature_categorical(df_bursts, 'time_rdsym', group_by='epoch', ylabel='Rise
 # --------------------------------------------
 #
 # In this example, we will compute features for a 2D array in which we have organized multiple
-# copies of the same signal. This is done to explore applying different thresholds while taking
-# advantage of parallel processing capabilities. This example also shows how a list of dictionaries
-# may be used to pass unique compute_features_kwargs to each signal.
+# copies of the same signal. Since continutity is not preserved, ``axis=1`` is appropriate. This
+# allows for the exploration of different thresholds while taking advantage of parallel processing.
+# This example also shows how a list of dictionaries may be used to pass unique
+# ``compute_features_kwargs``` to each signal.
 
 ####################################################################################################
 
@@ -152,7 +156,7 @@ compute_kwargs = [{'threshold_kwargs': dict(thresholds, amp_consistency_threshol
 
 # Compute features
 df_features = compute_features_2d(sigs, fs, f_range, compute_features_kwargs=compute_kwargs,
-                                  axis=1, n_jobs=-1, progress="tqdm")
+                                  axis=0, n_jobs=-1, progress="tqdm")
 
 ####################################################################################################
 
@@ -163,170 +167,3 @@ fig = plt.figure(figsize=(8, 8))
 plt.plot(amp_consist_threshes, n_bursts, marker="o")
 plt.xlabel("Amplitude Consistency Threshold")
 plt.ylabel("Number of Bursts")
-
-"""
-4. Running Bycycle on 2D Arrays
-===============================
-
-Compute bycycle features for 2D organizations of timeseries.
-
-Bycycle supports computing the features of 2D signals using :func:`~.compute_features_2d`.
-Signals may be organized in a different ways, including (n_epochs, n_timepoints) or
-(n_channels, n_timepoints). The difference between these organizataions is that continuity is
-preserved across epochs, but not across channels. The ``axis`` argument is used to specificy either
-continuous (``axis=1``) and non-continuous (``axis=None``) recordings. This argument functions
-identically to numpy functions, such as np.mean or np.sum.
-"""
-
-####################################################################################################
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from neurodsp.sim import sim_combined
-from bycycle.group import compute_features_2d
-from bycycle.plts import plot_feature_categorical
-
-####################################################################################################
-# The ``axis`` Argument
-# ---------------------
-#
-# Here, we will examine an analogous ``np.sum`` example of the axis argument before moving
-# onto specific bycycle examples. The axis argument be may specified as:
-#
-# - ``axis=1`` : Operates across each row/signal in an array independently (i.e. for each
-#   channel in (n_channels, n_timepoints)).
-# - ``axis=None`` : Operates on flattened rows/signals (i.e. across flatten epochs
-#   in (n_epochs, n_timepoints)).
-#
-
-####################################################################################################
-
-# Analagous to (n_channels, n_timepoints) or (n_epochs, n_timepoints)
-arr = np.ones((2, 3))
-
-print(arr, "\n")
-print("axis=1    (channels): ", np.sum(arr, axis=1))
-print("axis=None (epochs)  : ", np.sum(arr, axis=None))
-
-####################################################################################################
-#
-# Example 1. 2D Array (n_epochs, n_timepoints)
-# --------------------------------------------
-# The features for a 2d array with a (n_epochs, n_timepoints) organization will be computed here.
-# This is an example of when using ``axis = None`` is appropriate, assuming each epoch was recorded
-# continuously. Data will be simulated to produce to alternating epoch types (rest and task)
-# with varying rise-decay symmetries.
-
-####################################################################################################
-
-# Simulation settings
-n_seconds = 10
-fs = 500
-
-freq = 10
-f_range = (5, 15)
-
-epochs = 10
-
-# Define rdsym values for rest and task trials
-rdsym_rest = 0.5
-rdsym_task = 0.75
-
-####################################################################################################
-
-# Simulate a timeseries
-sigs = np.zeros((epochs, n_seconds*fs))
-
-for idx in range(epochs):
-
-    if idx % 2 == 0:
-        # Rest epoch
-        sim_components = {'sim_powerlaw': dict(exponent=-2),
-                          'sim_bursty_oscillation': dict(freq=10, cycle='asine',
-                                                         enter_burst=0.75, rdsym=rdsym_rest)}
-    else:
-        # Task epoch
-        sim_components = {'sim_powerlaw': dict(exponent=-2),
-                          'sim_bursty_oscillation': dict(freq=10, cycle='asine',
-                                                         enter_burst=0.75, rdsym=rdsym_task)}
-
-    sigs[idx] = sim_combined(n_seconds, fs, components=sim_components)
-
-####################################################################################################
-
-# Compute features with default bycycle thresholds
-thresholds = dict(amp_fraction_threshold=0., amp_consistency_threshold=.5,
-                  period_consistency_threshold=.5, monotonicity_threshold=.8,
-                  min_n_cycles=3)
-
-compute_kwargs = {'burst_method': 'cycles', 'threshold_kwargs': thresholds}
-
-df_features = compute_features_2d(sigs, fs, f_range, axis=None,
-                                  compute_features_kwargs=compute_kwargs)
-
-####################################################################################################
-
-# Add a column specifying epoch type
-for idx, df in enumerate(df_features):
-    df['epoch'] = 'rest' if idx % 2 == 0 else 'task'
-
-# Merge into a single dataframe
-df_concat = pd.concat(df_features, axis=0, ignore_index=True)
-
-# Limit to bursting cycles
-df_bursts = df_concat[df_concat['is_burst'] == True]
-
-# Plot
-plot_feature_categorical(df_bursts, 'time_rdsym', group_by='epoch', ylabel='Rise-Decay Symmetry',
-                         xlabel=['Rest', 'Task'])
-
-####################################################################################################
-#
-# Example 2. 2D Array (n_copies, n_timepoints)
-# --------------------------------------------
-#
-# In this example, we will compute features for a 2D array in which we have organized multiple
-# copies of the same signal. This is done to explore applying different thresholds while taking
-# advantage of parallel processing capabilities. This example also shows how a list of dictionaries
-# may be used to pass unique compute_features_kwargs to each signal.
-
-####################################################################################################
-
-# Simulate a timeseries
-n_copies = 5
-
-sim_components = {'sim_powerlaw': dict(exponent=-2),
-                  'sim_bursty_oscillation': dict(freq=10)}
-
-sig = sim_combined(n_seconds, fs, components=sim_components)
-
-sigs = np.array([sig] * n_copies)
-
-####################################################################################################
-
-# Step through amp_consistency thresholds by 0.25
-thresholds = dict(amp_fraction_threshold=0., amp_consistency_threshold=.5,
-                  period_consistency_threshold=.5, monotonicity_threshold=.8,
-                  min_n_cycles=3)
-
-amp_consist_threshes = [0, .25, .5, .75, 1.]
-
-compute_kwargs = [{'threshold_kwargs': dict(thresholds, amp_consistency_threshold=thresh)}
-                  for thresh in amp_consist_threshes]
-
-# Compute features
-df_features = compute_features_2d(sigs, fs, f_range, compute_features_kwargs=compute_kwargs,
-                                  axis=1, n_jobs=-1, progress="tqdm")
-
-####################################################################################################
-
-# Plot the number of detected burst at each threshold
-n_bursts = [np.count_nonzero(df_features[idx]['is_burst']) for idx in range(n_copies)]
-
-fig = plt.figure(figsize=(8, 8))
-plt.plot(amp_consist_threshes, n_bursts, marker="o")
-plt.xlabel("Amplitude Consistency Threshold")
-plt.ylabel("Number of Bursts")
-
--- tutorials/plot_5_3D_sigs.py
+++ tutorials/plot_5_3D_sigs.py
@@ -9,6 +9,7 @@ Signals may be organized in a different ways, including (n_participants, n_chann
 or (n_channels, n_epochs, n_timepoints). The difference between these organizations is that
 continuity may be assumed across (n_participants, n_channels) but not (n_channels, n_epochs). The
 ``axis`` argument is used to specificy the axis to iterate over in parallel.
+
 """
 
 ####################################################################################################
@@ -20,19 +21,20 @@ import matplotlib.pyplot as plt
 from neurodsp.sim import sim_combined
 from bycycle.group import compute_features_3d
 from bycycle.plts import plot_feature_categorical
+from bycycle.utils import flatten_dfs
 
 ####################################################################################################
-# The ``axis`` Argument
-# ---------------------
+# Example 1. The ``axis`` Argument
+# --------------------------------
 #
 # Here, we will show how the axis arguments works by iterating over slices of an 3D array.
 # The axis argument be may specified as:
 #
-# - ``axis=0`` : Operate over 2D slices along the zeroth dimension, (i.e. for each channel in
+# - ``axis=0`` : Iterates over 2D slices along the zeroth dimension, (i.e. for each channel in
 #   (n_channels, n_epochs, n_timepoints)).
-# - ``axis=1`` : Operates over 2D slices along the first dimension (i.e. across flatten epochs in
+# - ``axis=1`` : Iterates over 2D slices along the first dimension (i.e. across flatten epochs in
 #   (n_epochs, n_timepoints)).
-# - ``axis=(0, 1)`` : Operates along 1D slices along the zeroth and first dimension (i.e across
+# - ``axis=(0, 1)`` : Iterates over 1D slices along the zeroth and first dimension (i.e across
 #   each signal independently in (n_participants, n_channels, n_timepoints)).
 #
 
@@ -40,25 +42,25 @@ from bycycle.plts import plot_feature_categorical
 
 arr = np.ones((2, 3, 4))
 
-xlen = np.shape(arr)[0]
-ylen = np.shape(arr)[1]
+dim0_len = np.shape(arr)[0]
+dim1_len = np.shape(arr)[1]
 
 print("axis=0")
-for xdim in range(xlen):
-    print(np.shape(arr[xdim]))
+for dim0 in range(dim0_len):
+    print(np.shape(arr[dim0]))
 
 print("\naxis=1")
-for ydim in range(ylen):
-    print(np.shape(arr[:, ydim]))
+for dim1 in range(dim1_len):
+    print(np.shape(arr[:, dim1]))
 
 print("\naxis=(0, 1)")
-for xdim in range(xlen):
-    for ydim in range(ylen):
-        print(np.shape(arr[xdim, ydim]))
+for dim0 in range(dim0_len):
+    for dim1 in range(dim1_len):
+        print(np.shape(arr[dim0, dim1]))
 
 ####################################################################################################
 #
-# Example 3. 3D Array (n_channels, n_epochs, n_timepoints)
+# Example 2. 3D Array (n_channels, n_epochs, n_timepoints)
 # --------------------------------------------------------
 # The features from a 3d array of (n_channels, n_epochs, n_timepoints) will be computed here.
 # Bursting frequencies and rise-decay symmetry will be modulated across channels and epochs,
@@ -75,18 +77,22 @@ freq = 10
 f_range = (5, 15)
 
 n_channels = 5
-epochs = 10
+n_epochs = 10
+
+# Define rdsym values for rest and task trials
+rdsym_rest = 0.5
+rdsym_task = 0.75
 
 ####################################################################################################
 # Simulate a 3d timeseries
-sigs = np.zeros((n_channels, epochs, n_seconds*fs))
+sigs = np.zeros((n_channels, n_epochs, n_seconds*fs))
 freqs = np.linspace(5, 45, 5)
 
 for ch_idx, freq in zip(range(n_channels), freqs):
 
-    for ep_idx in range(epochs):
+    for ep_idx in range(n_epochs):
 
-        rdsym = 0.5 if ep_idx % 2 == 0 else 0.8
+        rdsym = rdsym_rest if ep_idx % 2 == 0 else rdsym_task
 
         sim_components = {'sim_powerlaw': dict(exponent=-2),
                           'sim_bursty_oscillation': dict(freq=freq, cycle='asine', rdsym=rdsym)}
@@ -95,36 +101,46 @@ for ch_idx, freq in zip(range(n_channels), freqs):
 
 ####################################################################################################
 
-# Compute features
+# Compute features with an higher than default period consistency threshold.
+#   This allows for more accurate estimates of burst frequency.
 thresholds = dict(amp_fraction_threshold=0., amp_consistency_threshold=.5,
-                  period_consistency_threshold=.5, monotonicity_threshold=.6,
+                  period_consistency_threshold=.8, monotonicity_threshold=.6,
                   min_n_cycles=3)
 
 compute_kwargs = {'burst_method': 'cycles', 'threshold_kwargs': thresholds}
 
-df_features = compute_features_3d(sigs, fs, (1, 50), axis=0,
-                                  compute_features_kwargs=compute_kwargs)
+dfs_features = compute_features_3d(sigs, fs, (1, 50), axis=0,
+                                   compute_features_kwargs=compute_kwargs)
 
 ####################################################################################################
 
-# Split dfs by epoch type
-df_rest = pd.concat([df_features[ch_idx][ep_idx] for ep_idx in range(epochs) if ep_idx % 2 == 0
-                     for ch_idx in range(n_channels)], axis=0, ignore_index=True)
-df_rest['epoch'] = 'rest'
-df_rest = df_rest[df_rest['is_burst'] == True]
+# Concatenate dataframes
+ep_labels = ['rest' if ep_idx % 2 == 0 else 'task'
+             for ch_idx in range(n_channels) for ep_idx in range(n_epochs)]
 
-df_task = pd.concat([df_features[ch_idx][ep_idx] for ep_idx in range(epochs) if ep_idx % 2 != 0
-                     for ch_idx in range(n_channels)], axis=0, ignore_index=True)
-df_task['epoch'] = 'task'
-df_task = df_task[df_task['is_burst'] == True]
+ch_labels = ["CH{ch_idx}".format(ch_idx=ch_idx, ep_idx=ep_idx)
+             for ch_idx in range(n_channels) for ep_idx in range(n_epochs)]
+
+df_epochs = flatten_dfs(dfs_features, ep_labels, 'Epoch')
+
+df_channels = flatten_dfs(dfs_features, ch_labels, 'Channel')
+
+# Limit to bursts
+df_epochs = df_epochs[df_epochs['is_burst'] == True]
+df_channels = df_channels[df_channels['is_burst'] == True]
 
 ####################################################################################################
 
-# See how well bycycle estimated each bursting cycles period across channels
-periods = [pd.concat(df_features[ch_idx], axis=0) for ch_idx in range(n_channels)]
-periods = [df[df['is_burst'] == True]['period'].mean() for df in periods]
+# Plot estimated frequency
+df_channels['freqs'] = fs / df_channels['period'].values
 
-freqs_est = [round(fs/ period, 3) for period in periods]
+plot_feature_categorical(df_channels, 'freqs', 'Channel', ylabel='Burst Frequency',
+                         xlabel=['CH00', 'CH01', 'CH02', 'CH03', 'CH04'])
+
+####################################################################################################
+
+# Compare estimated frequency to simulatated frequency
+freqs_est = df_channels.groupby('Channel').mean()['freqs'].values
 
 df_freqs = pd.DataFrame()
 df_freqs['Channel'] = ['CH_0{idx}'.format(idx=idx) for idx in range(n_channels)]
@@ -136,11 +152,14 @@ df_freqs
 ####################################################################################################
 
 # See how well bycycle estimated each bursting cycle's rise-decay symmetry within epochs
-df_rdsym = pd.DataFrame()
+rdsym_rest = df_epochs[df_epochs['Epoch'] == 'rest']['time_rdsym'].mean()
+rdsym_task = df_epochs[df_epochs['Epoch'] == 'task']['time_rdsym'].mean()
 
+df_rdsym = pd.DataFrame()
 df_rdsym['Epoch Type'] = ['Fixation', 'Task']
 df_rdsym['Simulated rdsym'] = [0.5, 0.75]
-df_rdsym['Calculated rdsym'] = [df_rest['time_rdsym'].mean(), df_task['time_rdsym'].mean()]
+
+df_rdsym['Calculated rdsym'] = [rdsym_rest, rdsym_task]
 df_rdsym['Error'] = np.abs(df_rdsym['Simulated rdsym'] - df_rdsym['Calculated rdsym'])
 df_rdsym
 
