a184026c65d7795b9596b3a854fae11c63279d39
ryanhammonds <rphammonds@ucsd.edu>
2023-03-30 14:55:18 -0700

    fix test from changing df.iterrows -> df.to_dict

--- bycycle/tests/features/test_shapes.py
+++ bycycle/tests/features/test_shapes.py
@@ -37,7 +37,7 @@ def test_compute_shape_features(sim_args, find_extrema_kwargs, center_extrema):
 
     # Assert that np.nan isn't in dataframe(s), with the exception of the first and last row
     for idx, row in enumerate(df_shapes.to_dict('records')):
-
+        row = list(row.values())
         assert not np.isnan(row[1:-1]).any()
 
     # Check inverted signal gives appropriately opposite data
