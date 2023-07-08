a18cbd7f2fcb80836ff60463735444a370305cef
Tom Donoghue <tdonoghue.research@gmail.com>
2023-07-08 12:17:43 -0400

    drop package_resources

--- bycycle/tests/settings.py
+++ bycycle/tests/settings.py
@@ -1,7 +1,7 @@
 """Settings for bycycle tests."""
 
 import os
-import pkg_resources as pkg
+from pathlib import Path
 
 ###################################################################################################
 ###################################################################################################
@@ -13,5 +13,6 @@ FREQ = 10
 F_RANGE = (6, 14)
 
 # Path Settings
-BASE_TEST_FILE_PATH = pkg.resource_filename(__name__, 'test_files')
+TESTS_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
+BASE_TEST_FILE_PATH = TESTS_PATH / 'test_files'
 TEST_PLOTS_PATH = os.path.join(BASE_TEST_FILE_PATH, 'plots')
