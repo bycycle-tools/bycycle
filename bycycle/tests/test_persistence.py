93ba20d194f47f090f344e22c62858eb72a3364e
Ryan Hammonds <ryanphammonds@gmail.com>
2024-05-28 16:28:02 -0700

    persistant hash updated

--- bycycle/tests/test_persistence.py
+++ bycycle/tests/test_persistence.py
@@ -7,7 +7,7 @@ import numpy as np
 ###################################################################################################
 
 # Update when a commit alters the result of compute_features
-PERSISTENT_HASH = '502ca93eee58f97caf83d42b866c9bab7f0074fb'
+PERSISTENT_HASH = 'b0781f4c317ec45056f1d0802a52eb51308f12f8'
 
 def test_persistent_features(sim_args_comb):
 
@@ -15,7 +15,7 @@ def test_persistent_features(sim_args_comb):
     df_features = sim_args_comb['df_features']
 
     # Convert to np array
-    arr_features = df_features.to_numpy().astype(np.float32).copy(order='C')
+    arr_features = df_features.to_numpy().astype(np.float32).round(12).copy(order='C')
 
     # Hash
     h = sha1()
